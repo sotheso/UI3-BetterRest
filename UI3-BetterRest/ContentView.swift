0d59bbaa408279b213f7a5ce7127f4cbd962e4d8
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-12-04 16:58:58 +0330

    تمرینی Date Time

--- UI3-BetterRest/ContentView.swift
+++ UI3-BetterRest/ContentView.swift
@@ -6,19 +6,31 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State private var SleepTime = 8.0
+    @State private var DateTime = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Stepper("\(SleepTime.formatted()) ساعت", value: $SleepTime, in: 4...12, step: 0.25)
+        
+        DatePicker("تاریخ", selection: $DateTime, displayedComponents: .hourAndMinute)
+            .labelsHidden()
+        
+        DatePicker("تاریخ", selection: $DateTime)
+            .labelsHidden()
+        
+        DatePicker("تاریخ", selection: $DateTime, in: Date.now...)
+            
+        Text(Date.now , format: .dateTime.day().month().year())
+        Text(Date.now.formatted(date: .long, time: .shortened))
+    }
+    
+    func exampledate(){
+        let complate = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = complate.hour ?? 0
+        let minute = complate.minute ?? 0
     }
 }
-
 #Preview {
     ContentView()
 }
