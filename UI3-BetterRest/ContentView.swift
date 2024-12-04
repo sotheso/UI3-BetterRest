13e75351b2a39d390dc23b7cc719ea6c356ecd1a
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-12-04 16:41:52 +0330

    Initial Commit

---
+++ UI3-BetterRest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UI3-BetterRest
+//
+//  Created by Sothesom on 14/09/1403.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UI3-BetterRest/UI3_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI3_BetterRestApp.swift
+//  UI3-BetterRest
+//
+//  Created by Sothesom on 14/09/1403.
+//
+
+import SwiftUI
+
+@main
+struct UI3_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
