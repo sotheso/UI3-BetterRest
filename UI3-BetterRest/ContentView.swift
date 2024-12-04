7dcb01a24110e5f1676f5fa418843a9bbe7bc103
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-12-04 18:16:53 +0330

    Edit 1

--- UI3-BetterRest/ContentView.swift
+++ UI3-BetterRest/ContentView.swift
@@ -27,34 +27,37 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             Form{
-                VStack (alignment: .leading, spacing: 0) {
-                    Text("کی میخوای بخوابی؟")
-                        .font(.headline)
-                    DatePicker("لطفا تایم خواب خود را مشخص کنید", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
+                Section {
+                    VStack (alignment: .leading, spacing: 0) {
+                        Text("کی میخوای بخوابی؟")
+                            .font(.headline)
+                        DatePicker("لطفا تایم خواب خود را مشخص کنید", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                    }
+                    
+                    VStack (alignment: .leading, spacing: 0) {
+                        Text("چقدر میخوای بخوابی؟")
+                            .font(.headline)
+                        Stepper("\(sleepAmount.formatted()) ساعت", value: $sleepAmount, in: 2...12 , step: 0.25)
+                    }
+                    
+                    VStack (alignment: .leading, spacing: 0) {
+                        Text("چند شات قهوه زدی؟")
+                            .font(.headline)
+                        Stepper(coffeeAmount == 1 ? "1 cup" : " \(coffeeAmount.formatted()) cups", value: $coffeeAmount, in: 1...12 , step: 1)
+                    }
                 }
-                
-                VStack (alignment: .leading, spacing: 0) {
-                    Text("چقدر میخوای بخوابی؟")
-                        .font(.headline)
-                    Stepper("\(sleepAmount.formatted()) ساعت", value: $sleepAmount, in: 2...12 , step: 0.25)
-                }
-                
-                VStack (alignment: .leading, spacing: 0) {
-                    Text("چند شات قهوه زدی؟")
-                        .font(.headline)
-                    Stepper(coffeeAmount == 1 ? "1 cup" : " \(coffeeAmount.formatted()) cups", value: $coffeeAmount, in: 1...12 , step: 1)
+                Section {
+                    Button("حساب کن", action: calculateBedtime)
                 }
             }
             .navigationTitle("Better Rest")
-            .toolbar(){
-                Button("حساب کن", action: calculateBedtime)
-            }
             .alert(alertTitle ,isPresented: $showingAlert) {
                 Button("OK") { }
             } message: {
                 Text(alerteMesseg )
             }
+            
         }
     }
     func calculateBedtime() {
